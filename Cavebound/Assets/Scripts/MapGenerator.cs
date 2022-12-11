c8eb9f616d93763f297faa86a239545fd633f304
Andrew Greenslade <andrew.greenslade71@yahoo.com>
2022-12-11 16:18:04 +0000

    added debug player spawns in shape of a square

--- Cavebound/Assets/Scripts/MapGenerator.cs
+++ Cavebound/Assets/Scripts/MapGenerator.cs
@@ -11,11 +11,19 @@ public class MapGenerator : NetworkBehaviour
 {
     [SyncVar]
     public float seed = 0;
+
     public int MapWidth = 5;
     public int MapHieght = 5;
+    public int MapSpawnCircle = 5;
+
     public Tilemap map;
+    public Tilemap BorderMap;
+    public Tilemap spawnMap;
+
     public Tile WhiteSquare;
     public Tile borderSquare;
+    public Tile BGSquare;
+    public Tile spawnSquare;
 
     public float NoiseScale = 1.0f;
     public int edgeSize = 10;
@@ -39,9 +47,11 @@ public class MapGenerator : NetworkBehaviour
         {
             for (int x = 0; x < MapWidth; x++)
             {
+                spawnMap.SetTile(new Vector3Int(x, y), BGSquare);
+
                 if (y <= edgeSize || y >= MapHieght - edgeSize || x <= edgeSize || x >= MapWidth - edgeSize)
                 {
-                    map.SetTile(new Vector3Int(x, y), borderSquare);
+                    BorderMap.SetTile(new Vector3Int(x, y), borderSquare);
                     continue;
                 }
 
@@ -54,6 +64,35 @@ public class MapGenerator : NetworkBehaviour
                 {
                     map.SetTile(new Vector3Int(x, y), WhiteSquare);
                 }
+            }
+        }
+
+        //Bottom left player gen
+        Vector3Int bottomLeft = new Vector3Int(edgeSize, edgeSize);
+        Vector3Int topLeft = new Vector3Int(edgeSize, MapHieght - MapSpawnCircle);
+        Vector3Int bottomRight = new Vector3Int(MapWidth - MapSpawnCircle, edgeSize);
+        Vector3Int topRight = new Vector3Int(MapWidth - MapSpawnCircle, MapHieght - MapSpawnCircle);
+
+        setPlayerSpawn(bottomRight);
+        setPlayerSpawn(bottomLeft);
+        setPlayerSpawn(topLeft);
+        setPlayerSpawn(topRight);
+    }
+
+    public void setPlayerSpawn(Vector3Int coords)
+    { 
+        for (int y = 0; y < MapSpawnCircle; y++)
+        {
+            for (int x = 0; x < MapSpawnCircle; x++)
+            {
+                if(x == 0 || y == 0 || x == MapSpawnCircle - 1 || y == MapSpawnCircle - 1)
+                {
+                    map.SetTile(new Vector3Int(coords.x + x, coords.y + y), WhiteSquare);
+                    continue;
+                }
+
+                spawnMap.SetTile(new Vector3Int(coords.x + x, coords.y + y), spawnSquare);
+                map.SetTile(new Vector3Int(coords.x + x, coords.y + y), null);
             }
         }
     }
