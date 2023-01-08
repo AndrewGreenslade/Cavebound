ec7f511ca35c4bf823401653f12567a56650dced
Andrew Greenslade <andrew.greenslade71@yahoo.com>
2023-01-08 16:03:12 +0000

    Added jumping mechanics, with effects, also flips the player when moving

--- Cavebound/Assets/Scripts/playerMove.cs
+++ Cavebound/Assets/Scripts/playerMove.cs
@@ -4,55 +4,87 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class playerMove : NetworkBehaviour
 {
     public float speed = 10.0f;
+    public float jumpForce = 100.0f;
+    public Transform raycastPosition;
+    public float jumpRayCastDistance;
+    public LayerMask groundLayer;
 
     private float movementX;
-    private float movementY;
-
-    private Camera cam;
+    private Vector2 origionalScale;
+    private CinemachineVirtualCamera myCam;
+    private Rigidbody2D rb;
 
     private float fireInput;
-    private float camSizeDefault;
-    public float camSizeUnzoomed;
+    private float jumpInput;
+    public ParticleSystem JumpParticleSys;
 
     private void Start()
     {
-        cam = GetComponentInChildren<Camera>();
-        camSizeDefault = cam.orthographicSize;
+        rb = GetComponent<Rigidbody2D>();
+        origionalScale = transform.localScale;
 
         if (!isLocalPlayer)
         {
             Destroy(GetComponent<PlayerInput>());
-            cam.gameObject.SetActive(false);
             this.enabled = false;
             return;
         }
+
+        myCam = FindObjectOfType<CinemachineVirtualCamera>();
+        myCam.Follow = transform;
     }
 
     private void Update()
     {
+        Debug.DrawRay(raycastPosition.position, Vector2.down * jumpRayCastDistance, Color.red);
+
+        if(movementX > 0)
+        {
+            transform.localScale = new Vector2(origionalScale.x,origionalScale.y);
+        }
+        else if (movementX < 0)
+        {
+            transform.localScale = new Vector2(-origionalScale.x, origionalScale.y);
+        }
+
+
         if (!isLocalPlayer)
         {
+
             return;
         }
-     
-        transform.position += new Vector3(movementX, movementY, 0) * Time.deltaTime * speed;
-        camZoom();
+        
+        transform.position += new Vector3(movementX, 0, 0) * Time.deltaTime * speed;
+        
+        if(jumpInput > 0)
+        {
+            if (isGrounded())
+            {
+                JumpParticleSys.enableEmission = true;
+                rb.AddForce(Vector2.up * jumpForce,ForceMode2D.Impulse);
+            }
+        }
+
+        if (isGrounded() && jumpInput == 0)
+        {
+            JumpParticleSys.enableEmission = false;
+        }
     }
 
-    private void camZoom()
+    private bool isGrounded()
     {
-        if (fireInput > 0)
+        RaycastHit2D ray = Physics2D.Raycast(raycastPosition.position, Vector2.down, jumpRayCastDistance, groundLayer);
+        if(ray.collider != null)
         {
-            cam.orthographicSize = camSizeUnzoomed;
+            return true;
         }
-        else
-        {
-            cam.orthographicSize = camSizeDefault;
-        }
+
+        return false;
     }
 
     void OnFire(InputValue t_fireVal)
@@ -64,6 +96,10 @@ public class playerMove : NetworkBehaviour
     {
         Vector2 moveVec = t_movementVal.Get<Vector2>();
         movementX = moveVec.x;
-        movementY = moveVec.y;
+    }
+
+    void OnJump(InputValue t_jumpVal)
+    {
+        jumpInput= t_jumpVal.Get<float>();
     }
 }
